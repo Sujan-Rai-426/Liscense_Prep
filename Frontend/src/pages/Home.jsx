7efd29ccba71273fc0b9a40da026de050620aa22
Sujan <rsujan140.in@gmail.com>
2025-04-29 19:57:38 +0545

    minor edit. --> commit no.24

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -46,7 +46,9 @@ function Home(props) {
     return (
         <div className="container mt-0 py-4">
             <div className='my-3 text-center text-danger fw-bold fs-4">'>
-                <b> --Some of the BCT License Past Questions chapter wise -- </b>
+                <b> Some of the BCT License Past Questions chapter wise </b>
+                                    <br />
+                <i> -- <u> More questions and contents  will be added very soon </u> -- </i>
             </div>
             {loading ? (
                 renderSkeletons()
